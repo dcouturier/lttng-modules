f2701de395a20c0a031e9cc2ce9275a431285a2c
dcouturier <david.couturier@polymtl.ca>
2015-02-03 17:37:33 -0500

    New probes for the intel_gpu_freq_change
    Probe that track changes in the GPU frequency for Intel GPUs.
    More tracepoints for the i915 driver are to come: the same file
    architecture will be used for monitoring the GPU's execution
    queue.
    
    Signed-off-by: David Couturier <david.couturier@polymtl.ca>

---
+++ instrumentation/events/lttng-module/i915.h
@@ -0,0 +1,29 @@
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM i915
+
+#if !defined(LTTNG_TRACE_I915_H) || defined(TRACE_HEADER_MULTI_READ)
+#define LTTNG_TRACE_I915_H
+
+#include "../../../probes/lttng-tracepoint-event.h"
+
+LTTNG_TRACEPOINT_EVENT(intel_gpu_freq_change,
+	
+	TP_PROTO(u32 freq),
+
+	TP_ARGS(freq),
+
+	TP_STRUCT__entry(
+		__field(u32, freq)
+	),
+
+	TP_fast_assign(
+		tp_assign(freq, freq)
+	),
+
+	TP_printk("new_freq=%u",__entry->freq)
+)
+
+#endif /* LTTNG_TRACE_I915_H */
+
+/* This part must be outside protection */
+#include "../../../probes/define_trace.h"
---
+++ probes/lttng-probe-i915.c
@@ -0,0 +1,51 @@
+/*
+ * probes/lttng-probe-i915.c
+ *
+ * LTTng probes for Intel i915 drivers.
+ *
+ * Copyright (C) 2014-2015 David Couturier <david.couturier@polymtl.ca>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; only
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <linux/module.h>
+#include <linux/interrupt.h>
+#include "../lttng-tracer.h"
+
+
+/*
+ * Create the tracepoint static inlines from the kernel to validate that our
+ * trace event macros match the kernel we run on.
+ */
+//#include <trace/events/i915.h>
+
+#include "../wrapper/tracepoint.h"
+
+/*
+ * Create LTTng tracepoint probes.
+ */
+#define LTTNG_PACKAGE_BUILD
+#define CREATE_TRACE_POINTS
+#define TRACE_INCLUDE_PATH ../instrumentation/events/lttng-module
+
+#include "../instrumentation/events/lttng-module/i915.h"
+
+MODULE_LICENSE("GPL and additional rights");
+MODULE_AUTHOR("David Couturier <david.couturier@polymtl.ca>");
+MODULE_DESCRIPTION("LTTng probes for Intel i915 drivers");
+MODULE_VERSION(__stringify(LTTNG_MODULES_MAJOR_VERSION) "."
+	__stringify(LTTNG_MODULES_MINOR_VERSION) "."
+	__stringify(LTTNG_MODULES_PATCHLEVEL_VERSION)
+	LTTNG_MODULES_EXTRAVERSION);
